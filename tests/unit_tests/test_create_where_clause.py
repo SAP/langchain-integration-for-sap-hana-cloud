2dbd6d2c527ef6ecad4be30934097a84172d6891
Varin Thakur <varin.thakur@sap.com>
2026-02-17 12:57:59 +0000

    add validation checks for operators and support null value filtering

--- tests/unit_tests/test_create_where_clause.py
+++ tests/unit_tests/test_create_where_clause.py
@@ -1,8 +1,9 @@
 import pytest
+import re
 from typing import List, Dict, Any
 from langchain_hana.vectorstores.create_where_clause import CreateWhereClause 
 from langchain_hana.vectorstores.hana_db import default_metadata_column
-from tests.integration_tests.fixtures.filtering_test_cases import FILTERING_TEST_CASES
+from tests.integration_tests.fixtures.filtering_test_cases import FILTERING_TEST_CASES, ERROR_FILTERING_TEST_CASES
 
 class MockHanaDb:
     def __init__(self):
@@ -27,6 +28,16 @@ def test_create_where_clause_unsupported_filter_value_type() -> None:
     with pytest.raises(ValueError, match="Unsupported filter value type"):
         CreateWhereClause(MockHanaDb())(unsupported_filter)
 
+@pytest.mark.parametrize(
+    "test_filter, expected_exception_message",
+    ERROR_FILTERING_TEST_CASES,
+)
+def test_create_where_clause_invalid_filters(
+    test_filter: Dict[str, Any],
+    expected_exception_message: str,
+) -> None:
+    with pytest.raises(ValueError, match=re.escape(expected_exception_message)):
+        CreateWhereClause(MockHanaDb())(test_filter)
 
 @pytest.mark.parametrize(
     "test_filter, expected_ids, expected_where_clause, expected_where_clause_parameters",
