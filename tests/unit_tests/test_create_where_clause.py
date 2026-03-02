e195bc8c1f9f06143f8be9aff98943800c3b2497
Varin Thakur <varin.thakur@sap.com>
2026-03-02 14:50:21 +0000

    Revert validation changes

--- tests/unit_tests/test_create_where_clause.py
+++ tests/unit_tests/test_create_where_clause.py
@@ -3,7 +3,7 @@ import re
 from typing import List, Dict, Any
 from langchain_hana.vectorstores.create_where_clause import CreateWhereClause 
 from langchain_hana.vectorstores.hana_db import default_metadata_column
-from tests.integration_tests.fixtures.filtering_test_cases import FILTERING_TEST_CASES, ERROR_FILTERING_TEST_CASES
+from tests.integration_tests.fixtures.filtering_test_cases import FILTERING_TEST_CASES
 
 class MockHanaDb:
     def __init__(self):
@@ -16,16 +16,18 @@ def test_create_where_clause_empty_filter() -> None:
     assert where_clause == ""
     assert parameters == []
 
-@pytest.mark.parametrize(
-    "test_filter, expected_exception_message",
-    ERROR_FILTERING_TEST_CASES,
-)
-def test_create_where_clause_invalid_filters(
-    test_filter: Dict[str, Any],
-    expected_exception_message: str,
-) -> None:
-    with pytest.raises(ValueError, match=re.escape(expected_exception_message)):
-        CreateWhereClause(MockHanaDb())(test_filter)
+
+def test_create_where_clause_unexpected_operator() -> None:
+    invalid_filter = {"$eq": [{"key": "value"}]}
+    with pytest.raises(ValueError, match="Unexpected operator"):
+        CreateWhereClause(MockHanaDb())(invalid_filter)
+
+
+def test_create_where_clause_unsupported_filter_value_type() -> None:
+    unsupported_filter = {"key": [1, 2, 3]}
+    with pytest.raises(ValueError, match="Unsupported filter value type"):
+        CreateWhereClause(MockHanaDb())(unsupported_filter)
+
 
 @pytest.mark.parametrize(
     "test_filter, expected_ids, expected_where_clause, expected_where_clause_parameters",
