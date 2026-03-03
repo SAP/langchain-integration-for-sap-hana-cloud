bfcf4e6a032fc72d2a6848572a95e58ceb7e7dae
Varin Thakur <varin.thakur@sap.com>
2026-03-03 11:56:30 +0000

    add constructor

--- langchain_hana/agents/graph_qa/hana_sparql_qa_agent.py
+++ langchain_hana/agents/graph_qa/hana_sparql_qa_agent.py
@@ -1,4 +1,17 @@
+from langchain_core.language_models import BaseLanguageModel
+from langchain_hana import HanaRdfGraph
 class HanaSparqlQAAgent:
   """Agent for answering questions using SPARQL and HANA RDF Graphs"""
 
-  __
+  SYSTEM_PROMPT = """
+  """
+
+  def __init__(
+      self,
+      graph: HanaRdfGraph,
+      llm: BaseLanguageModel,
+      system_prompt = SYSTEM_PROMPT,
+  ):
+    self.graph = graph
+    self.llm = llm
+    self.system_prompt = system_prompt
