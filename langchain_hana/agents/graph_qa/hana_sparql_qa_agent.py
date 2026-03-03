07b43e71974962275cb77a0e7607857daadadc57
Varin Thakur <varin.thakur@sap.com>
2026-03-03 14:28:03 +0000

    Add initial logic

--- langchain_hana/agents/graph_qa/hana_sparql_qa_agent.py
+++ langchain_hana/agents/graph_qa/hana_sparql_qa_agent.py
@@ -1,17 +1,63 @@
 from langchain_core.language_models import BaseLanguageModel
+from langchain.tools import tool
+from langchain.agents import create_agent
+from langchain.agents.middleware import ModelCallLimitMiddleware
 from langchain_hana import HanaRdfGraph
+from langgraph.prebuilt import ToolNode
+from prompts import SYSTEM_PROMPT
+
 class HanaSparqlQAAgent:
-  """Agent for answering questions using SPARQL and HANA RDF Graphs"""
+    """Agent for answering questions using SPARQL and HANA RDF Graphs"""
 
-  SYSTEM_PROMPT = """
-  """
+    def __init__(self, graph: HanaRdfGraph, model: BaseLanguageModel, tools=None, middleware=None, system_prompt: str = None, include_default_tools: bool = True, include_default_middleware: bool = True):
+        self.graph = graph
+        self.ontology = self.graph.get_ontology().serialize(format="turtle")
+        self.system_prompt = system_prompt or SYSTEM_PROMPT.format(self.graph.graph_uri)
+        self.include_default_tools = include_default_tools
+        self.include_default_middleware = include_default_middleware
 
-  def __init__(
-      self,
-      graph: HanaRdfGraph,
-      llm: BaseLanguageModel,
-      system_prompt = SYSTEM_PROMPT,
-  ):
-    self.graph = graph
-    self.llm = llm
-    self.system_prompt = system_prompt
+        # Create tools bound to this instance
+        if tools:
+            self.tools = tools
+        else:
+            self.tools =[]
+        
+        if self.include_default_tools:
+            self.tools.extend([self._create_ontology_tool(), self._create_sparql_tool()])
+
+        # Create the middleware
+        if middleware:
+          self.middleware = middleware
+        else:
+          self.middleware = []
+
+        if self.include_default_middleware:
+          self.middleware.append(ModelCallLimitMiddleware(run_limit=10))
+
+    def _create_ontology_tool(self):
+        @tool
+        def retrieve_ontology() -> str:
+            """Retrieve ontology from the HANA RDF Graph"""
+            return f"Ontology Information:\n{self.ontology}"
+        return retrieve_ontology
+    
+    def _create_sparql_tool(self):
+        @tool
+        def execute_sparql(query: str) -> str:
+            """Query the HANA RDF graph and return the fetched triples as a string.
+            Args:
+                query: SPARQL query to execute on the RDF graph
+            """
+            try:
+              query_result = self.graph.query(query)
+            except Exception as e:
+                return f"Error executing SPARQL query: {e}"   
+            return f"SPARQL Query Result:\n{query_result}"
+        return execute_sparql
+    
+    @classmethod
+    def create_agent(cls, graph: HanaRdfGraph, model: BaseLanguageModel, tools=None, system_prompt: str = None, middleware=None, **kwargs):
+        """Create a new SPARQL QA agent instance"""
+        instance = cls(graph, model, system_prompt)
+        tool_node = ToolNode(instance.tools, handle_tool_errors=True)
+        return create_agent(model, tools=tool_node, system_prompt=instance.system_prompt, middleware=middleware, **kwargs)
