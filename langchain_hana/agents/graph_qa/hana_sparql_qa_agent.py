90a00baccbbce61837c6a59c8236eaadd1795857
Varin Thakur <varin.thakur@sap.com>
2026-05-20 13:20:29 +0000

    add types and format

--- langchain_hana/agents/graph_qa/hana_sparql_qa_agent.py
+++ langchain_hana/agents/graph_qa/hana_sparql_qa_agent.py
@@ -1,44 +1,65 @@
-from langchain_core.language_models import BaseLanguageModel
-from langchain.tools import tool
+from typing import Any, Callable, Sequence
+
 from langchain.agents import create_agent
 from langchain.agents.middleware import ModelCallLimitMiddleware
-from langchain_hana import HanaRdfGraph
-from langgraph.prebuilt import ToolNode
+from langchain.agents.middleware.types import AgentMiddleware, ContextT, StateT_co
+from langchain.tools import BaseTool, tool
+from langchain_core.language_models import BaseLanguageModel
+from langchain_core.messages import SystemMessage
+
+from langchain_hana.graphs import HanaRdfGraph
+
 from .prompts import SYSTEM_PROMPT
+
 
 class HanaSparqlQAAgent:
     """Agent for answering questions using SPARQL and HANA RDF Graphs"""
 
-    def __init__(self, graph: HanaRdfGraph, tools=None, middleware=None, system_prompt: str = None, include_default_tools: bool = True, include_default_middleware: bool = True):
+    def __init__(
+        self,
+        graph: HanaRdfGraph,
+        tools: Sequence[BaseTool | Callable[..., Any] | dict[str, Any]] | None = None,
+        middleware: Sequence[AgentMiddleware[StateT_co, ContextT]] = (),
+        system_prompt: str | SystemMessage | None = None,
+        include_default_tools: bool = True,
+        include_default_middleware: bool = True,
+    ):
         self.graph = graph
         self.ontology = self.graph.get_schema.serialize(format="turtle")
-        self.system_prompt = system_prompt or SYSTEM_PROMPT.format(self.graph.from_clause)
+
+        if system_prompt is None:
+            self.system_prompt = SYSTEM_PROMPT.format(self.graph.from_clause)
+        else:
+            self.system_prompt = system_prompt
 
         # Create tools bound to this instance
         if tools:
             self.tools = tools
         else:
-            self.tools =[]
-        
+            self.tools = []
+
         if include_default_tools:
-            self.tools.extend([self._create_ontology_tool(), self._create_sparql_tool()])
+            self.tools.extend(
+                [self._create_ontology_tool(), self._create_sparql_tool()]
+            )
 
         # Create the middleware
         if middleware:
-          self.middleware = middleware
+            self.middleware = middleware
         else:
-          self.middleware = []
+            self.middleware = []
 
         if include_default_middleware:
-          self.middleware.append(ModelCallLimitMiddleware(run_limit=10))
+            self.middleware.append(ModelCallLimitMiddleware(run_limit=10))
 
     def _create_ontology_tool(self):
         @tool
         def retrieve_ontology() -> str:
             """Retrieve ontology from the HANA RDF Graph"""
             return f"Ontology Information:\n{self.ontology}"
+
         return retrieve_ontology
-    
+
     def _create_sparql_tool(self):
         @tool
         def execute_sparql(query: str) -> str:
@@ -47,14 +68,31 @@ class HanaSparqlQAAgent:
                 query: SPARQL query to execute on the RDF graph
             """
             try:
-              query_result = self.graph.query(query)
+                query_result = self.graph.query(query)
             except Exception as e:
-                return f"Error executing SPARQL query: {e}"   
+                return f"Error executing SPARQL query: {e}"
             return f"SPARQL Query Result:\n{query_result}"
+
         return execute_sparql
-    
+
     @classmethod
-    def create_agent(cls, graph: HanaRdfGraph, model: BaseLanguageModel, tools=None, system_prompt: str = None, middleware=None, **kwargs):
+    def create_agent(
+        cls,
+        graph: HanaRdfGraph,
+        model: BaseLanguageModel,
+        tools: Sequence[BaseTool | Callable[..., Any] | dict[str, Any]] | None = None,
+        system_prompt:  str | SystemMessage | None = SYSTEM_PROMPT,
+        middleware: Sequence[AgentMiddleware[StateT_co, ContextT]] = (),
+        **kwargs,
+    ):
         """Create a new SPARQL QA agent instance"""
-        instance = cls(graph=graph, tools=tools, middleware=middleware, system_prompt=system_prompt)
-        return create_agent(model, tools=instance.tools, system_prompt=instance.system_prompt, middleware=instance.middleware, **kwargs)
+        instance = cls(
+            graph=graph, tools=tools, middleware=middleware, system_prompt=system_prompt
+        )
+        return create_agent(
+            model,
+            tools=instance.tools,
+            system_prompt=instance.system_prompt,
+            middleware=instance.middleware,
+            **kwargs,
+        )
